3cf317a5139b8ef58bd39604f695768112571fce
Lynette Nguyen <lynettenguyen0909@gmail.com>
2020-05-27 18:41:25 -0700

    Made changes to navbar logo

--- website/src/components/Contact/Contact.js
+++ website/src/components/Contact/Contact.js
@@ -1,5 +1,15 @@
 import React from "react";
-
-export default function Contact() {
-  return <div className='contact'>This is the Contact Page!</div>;
+import Header from "../Page/Header";
+import Navbar from "../Page/Navbar";
+import Footer from "../Page/Footer";
+function Contact() {
+  return (
+    <div className='Contact'>
+      <Navbar />
+      <Header />
+      <Footer />
+    </div>
+  );
 }
+
+export default Contact;

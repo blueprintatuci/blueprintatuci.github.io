6ad5a40d8252d103bf812148b30de5c5430ec192
areetaw <areetaw@gmail.com>
2020-05-27 21:07:49 -0700

    included about us and mission sections

--- website/src/components/About/About.js
+++ website/src/components/About/About.js
@@ -1,16 +1,16 @@
 import React from "react";
 import AboutUsSection from "./AboutUsSection";
+import OurMissionSection from "./OurMissionSection"
 import Navbar from "../Page/Navbar";
 import Footer from "../Page/Footer";
 import "../../base.css";
-
-//import { makeStyles } from "@material-ui/core/styles";
 
 function About() {
   return (
     <div className='About'>
       <Navbar />
       <AboutUsSection />
+      <OurMissionSection />
       <Footer />
     </div>
   );
---
+++ website/src/components/About/OurMissionSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './OurMissionSection.css'
+
+//import { makeStyles } from "@material-ui/core/styles";
+
+export default function OurMissionSection() {
+  return (
+    <div className='our-mission-section'>
+      <h1 className='title'>Our Mission</h1>
+      <p className='mission-statement'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut.
+      </p>
+    </div>
+  );
+}

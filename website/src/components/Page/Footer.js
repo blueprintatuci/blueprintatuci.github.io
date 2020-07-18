e289cbcf9b43fd6264d79b8bff5c012c88ecde57
Justin Huynh <Justin@MacBook-Pro.local>
2020-07-18 13:13:42 -0700

    Redid footer to make it responsive

--- website/src/components/Page/Footer.js
+++ website/src/components/Page/Footer.js
@@ -1,116 +1,46 @@
 import React from "react";
 import "./Footer.css";
 import blueprint_logo from "../../assets/blueprint_logo_only.png";
-import facebook_logo from "../../assets/facebook.svg";
-import instagram_logo from "../../assets/instagram.svg";
-import github_logo from "../../assets/github.svg";
-import linkedin_logo from "../../assets/linkedin.svg";
-import { Link } from "react-router-dom";
 //import { makeStyles } from "@material-ui/core/styles";
 
 export default function Footer() {
   return (
-    <div className='footer'>
-      <div class='container'>
-        <div class='row'>
-          <div class='column left'>
-          <div class='footer-logo'>
-            <Link style={{ textDecoration: 'none' }} to='/'>
-              
-              <img src={blueprint_logo} alt='blueprint-logo' />
-              <div>blueprint</div>
-            </Link>
+    <div class="footer">
+        <div class="inner-footer">
+
+            <div class="footer-logo">
+                <img src={blueprint_logo} alt='blueprint-logo' />
+                <h1>blueprint</h1>
             </div>
-          </div>
-          <div class='column middle'>
-            <div class='headings'>General</div>
-            <ul>
-              <li>
-                <a class='link' href='/'>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a class='link' href='/about'>
-                  About
-                </a>
-              </li>
-              <li>
-                <a class='link' href='/projects'>
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a class='link' href='/contact'>
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class='column right'>
-            <div class='headings'>Social Media</div>
-            <div class='inner-column inner-column-left'>
-              <ul class='logo-column'>
-                <li class='logo-column-margin-bot'>
-                  <div class='logo'>
-                    <a href='http://facebook.com/uciblueprint'>
-                      <img src={facebook_logo} alt='facebook-logo' />
-                    </a>
-                  </div>
-                </li>
-                <li class='logo-column-margin-bot'>
-                  <div class='logo'>
-                    <a href='http://instagram.com/uciblueprint'>
-                      <img src={instagram_logo} alt='instagram-logo' />
-                    </a>
-                  </div>
-                </li>
-                <li class='logo-column-margin-bot'>
-                  <div class='logo'>
-                    <a href='http://github.com/uciblueprint'>
-                      <img src={github_logo} alt='github-logo' />
-                    </a>
-                  </div>
-                </li>
-                <li class='logo-column-margin-bot'>
-                  <div class='logo'>
-                    <a href='http://linkedin.com/company/uciblueprint/'>
-                      <img src={linkedin_logo} alt='linkedin-logo' />
-                    </a>
-                  </div>
-                </li>
-              </ul>
+            
+            <div class="footer-items">
+                
             </div>
-            <div class='inner-column inner-column-right'>
-              <ul>
-                <li>
-                  <a class='link' href='http://facebook.com/uciblueprint'>
-                    Facebook
-                  </a>
-                </li>
-                <li>
-                  <a class='link' href='http://instagram.com/uciblueprint'>
-                    Instagram
-                  </a>
-                </li>
-                <li>
-                  <a class='link' href='http://github.com/uciblueprint'>
-                    Github
-                  </a>
-                </li>
-                <li>
-                  <a class='link' href='http://linkedin.com/company/uciblueprint/'>
-                    LinkedIn
-                  </a>
-                </li>
-              </ul>
+        
+            <div class="footer-items">
+                <h2>General</h2>
+                <ul>
+                    <a href="/"><li>Home</li></a>
+                    <a href="/about"><li>About</li></a>
+                    <a href="/projects"><li>Projects</li></a>
+                    <a href="/contact"><li>Contact</li></a>
+                </ul>
             </div>
-          </div>
+            
+            <div class="footer-items">
+                <h2>Social Media</h2>
+                <ul>
+                    <a href="http://facebook.com/uciblueprint"><li><i class="fa fa-facebook fa-lg" aria-hidden="true"></i>Facebook</li></a>
+                    <a href="http://instagram.com/uciblueprint"><li><i class="fa fa-instagram fa-lg" aria-hidden="true"></i>Instagram</li></a>
+                    <a href="http://github.com/uciblueprint"><li><i class="fa fa-github fa-lg" aria-hidden="true"></i>Github</li></a>
+                    <a href="http://linkedin.com/company/uciblueprint/"><li><i class="fa fa-linkedin-square fa-lg" aria-hidden="true"></i>LinkedIn</li></a>
+                </ul>
+            </div>
+        
         </div>
-      </div>
-      <div class='copyright'>
-        <p>© Blueprint 2020</p>
-      </div>
+        <div class="footer-bottom">
+            © Blueprint 2020
+        </div>
     </div>
   );
 }

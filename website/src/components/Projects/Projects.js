733e3f9f64e28975074a0f77e3835596712535bb
Jeffrey Xu <Jeffrey@smilefish.com>
2020-09-25 17:33:42 -0700

    Responsive Navbar

--- website/src/components/Projects/Projects.js
+++ website/src/components/Projects/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import "../../base.css";
 import "./Projects.css";
 import HeaderSection from "../common/HeaderSection";
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import Navbar from "../Page/Navbar";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import BlueprintNavbar from "../Page/Navbar";
 import Footer from "../Page/Footer";
 import ProjectCard from "../common/ProjectCard";
 import "../Home/ProjectSection.css";
@@ -15,15 +15,19 @@ import ethic_logo from "../../assets/ethic_logo.png";
 function Projects() {
   return (
     <div className='Projects'>
-      <Navbar />
-      <HeaderSection 
-        title = "Projects"
-        info = "Hearties long boat crow's nest parrel Pirate Round line yardarm loot coffer 
+      <BlueprintNavbar />
+      <HeaderSection
+        title='Projects'
+        info="Hearties long boat crow's nest parrel Pirate Round line yardarm loot coffer 
         stern. Lass plunder furl pillage knave matey jack reef interloper belay. Gangplank 
         dead men tell no tales hulk swab Privateer coffer sheet prow cackle fruit rope's end."
       />
       <div className='body-section'>
-        <Button variant='contained' disableElevation href="https://github.com/uciblueprint">
+        <Button
+          variant='contained'
+          disableElevation
+          href='https://github.com/uciblueprint'
+        >
           See our GitHub
         </Button>
         {/* Uncomment below when we have projects we are working on */}
@@ -35,13 +39,13 @@ function Projects() {
         </div> */}
         <h1>Completed Projects</h1>
         <div className='projects'>
-          <ProjectCard 
-            pic = {ethic_logo}
-            title = "Ethic"
-            description = "Ethic provides resources to make it easier to live and shop sustainably.
+          <ProjectCard
+            pic={ethic_logo}
+            title='Ethic'
+            description='Ethic provides resources to make it easier to live and shop sustainably.
             We built them a web crawler that provided a central location for Ethic to easily view
-            and promote sustainability resources."
-            github_url = "https://github.com/uciblueprint/Ethics-Project-Frontend"
+            and promote sustainability resources.'
+            github_url='https://github.com/uciblueprint/Ethics-Project-Frontend'
           />
           {/* <ProjectCard />
           <ProjectCard /> */}

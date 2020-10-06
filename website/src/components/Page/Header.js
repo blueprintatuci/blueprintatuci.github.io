abfe5c1a06ae38dab37bbb1da74b5b7babb638f4
areetaw <areetaw@gmail.com>
2020-10-06 14:41:47 -0700

    add in button for mailing list

--- website/src/components/Page/Header.js
+++ website/src/components/Page/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Header.css";
-//import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "react-bootstrap";
 
 export default function Header() {
   return (
     <div className='header'>
-      <h1 className='title'> Tech For Social Good</h1>
+      <h1 className='title'>Tech For Social Good</h1>
+      <Button variant="light" href="http://eepurl.com/hez_cj">Subscribe to our mailing list</Button>
     </div>
   );
 }

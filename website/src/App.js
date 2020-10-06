5b819a1aaedd1ee747ae335b1cc4a5a2ad4b27fb
Jeffrey Xu <Jeffrey@smilefish.com>
2020-10-06 08:42:56 -0700

    fixing deployed routing

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact"
-import Navbar from "./components/Page/Navbar"
+import Contact from "./components/Contact/Contact";
+import Navbar from "./components/Page/Navbar";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className='App'>
         <Navbar />
         <Switch>
-
           <Route path='/' exact component={Home} />
           <Route path='/about' exact component={About} />
           <Route path='/projects' exact component={Projects} />
           <Route path='/contact' exact component={Contact} />
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 

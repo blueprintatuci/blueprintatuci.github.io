bf3879b22106247bd738f86f1bcbf28ff7523279
areetaw <areetaw@gmail.com>
2020-10-06 21:16:41 -0700

    update to white outline

--- website/src/components/Page/Header.js
+++ website/src/components/Page/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className='header'>
       <h1 className='title'>Tech For Social Good</h1>
-      <Button variant="light" href="http://eepurl.com/hez_cj">Subscribe to our mailing list</Button>
+      <Button variant="outline-light" href="http://eepurl.com/hez_cj">Subscribe to our mailing list</Button>
     </div>
   );
 }

15f038b89139505398cf21f99c37a0aacc6f4870
Justin Huynh <Justin@MacBook-Pro-2.local>
2020-10-13 18:32:33 -0700

    Adding new banner to home page

--- src/components/Page/Header.js
+++ src/components/Page/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
 import { Button } from "react-bootstrap";
+import banner from "../../assets/banner_transparent-01.svg";
 
 export default function Header() {
   return (
     <div className='header'>
+      <img classname='banner' src={banner} alt="banner"/>
       <h1 className='title'>Tech For Social Good</h1>
       <Button variant="outline-light" href="http://eepurl.com/hez_cj">Subscribe to our mailing list</Button>
     </div>

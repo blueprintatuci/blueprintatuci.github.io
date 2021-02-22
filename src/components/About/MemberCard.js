b13700f607399ef105c0e241060fa766ca016559
areetaw <areetaw@gmail.com>
2021-02-21 20:11:22 -0800

    add new members and new projects

--- src/components/About/MemberCard.js
+++ src/components/About/MemberCard.js
@@ -5,7 +5,7 @@ import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import linkedin_logo from "../../assets/linkedin.svg";
+import linkedin_logo from "../../assets/social_media/linkedin.svg";
 
 const useStyles = makeStyles(theme => ({
   card: {

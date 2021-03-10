6d6c6855a015641bcc65fc320b69ea7c650a48e6
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-03-10 02:13:46 -0800

    Update footer links for new routing
    No longer need the #!

--- src/components/Page/Footer.js
+++ src/components/Page/Footer.js
@@ -21,9 +21,9 @@ export default function Footer() {
                 <h2>General</h2>
                 <ul>
                     <a href="/"><li>Home</li></a>
-                    <a href="/#/about"><li>About</li></a>
-                    <a href="/#/projects"><li>Projects</li></a>
-                    <a href="/#/contact"><li>Contact</li></a>
+                    <a href="/about"><li>About</li></a>
+                    <a href="/projects"><li>Projects</li></a>
+                    <a href="/contact"><li>Contact</li></a>
                 </ul>
             </div>
             
